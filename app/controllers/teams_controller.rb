de88b79e0019c24a408a32a6642b1e14f0c36c97
shihoimamura <zhibao29@yahoo.co.jp>
2023-02-01 16:49:09 +0900

    オーナーのみ設定

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -30,11 +30,15 @@ class TeamsController < ApplicationController
   end
 
   def update
-    if @team.update(team_params)
-      redirect_to @team, notice: I18n.t('views.messages.update_team')
+    if @team.owner == current_user
+      if @team.update(team_params)
+        redirect_to @team, notice: I18n.t('views.messages.update_team')
+      else
+        flash.now[:error] = I18n.t('views.messages.failed_to_save_team')
+        render :edit
+      end
     else
-      flash.now[:error] = I18n.t('views.messages.failed_to_save_team')
-      render :edit
+      redirect_to @team, notice: "チームオーナー以外は編集できません"
     end
   end
 
